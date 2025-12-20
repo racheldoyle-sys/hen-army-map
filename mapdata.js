d9def5a98ec4e2e61ba01429263978cdfbe10841
racheldoyle-sys <rachel.doyle@hennozzles.com>
2025-12-19 20:05:47 -0500

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,822 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    state_description: "",
+    state_color: "#e6e6e6",
+    state_hover_color: "#a5b9c4  ",
+    state_url: "",
+    border_size: "2.5",
+    all_states_inactive: "yes",
+    all_states_zoomable: "no",
+    location_description: "",
+    location_color: "#3076bc",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "45",
+    location_type: "marker",
+    location_image_source: "",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#fff",
+    label_hover_color: "#fff",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "yes",
+    hide_eastern_labels: "yes    ",
+    manual_zoom: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    zoom_out_incrementally: "no",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    back_image: "no",
+    initial_back: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "./map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    border_hover_color: ""
+  },
+  state_specific: {
+    HI: {
+      zoomable: "no",
+      name: "Hawaii"
+    },
+    AK: {
+      zoomable: "no",
+      name: "Alaska"
+    },
+    FL: {
+      zoomable: "no",
+      name: "Florida"
+    },
+    NH: {
+      zoomable: "no",
+      name: "New Hampshire"
+    },
+    VT: {
+      zoomable: "no",
+      name: "Vermont"
+    },
+    ME: {
+      zoomable: "no",
+      name: "Maine"
+    },
+    RI: {
+      zoomable: "no",
+      name: "Rhode Island"
+    },
+    CT: {
+      zoomable: "no",
+      name: "Connecticut"
+    },
+    MA: {
+      zoomable: "no",
+      name: "Massachusetts"
+    },
+    NY: {
+      zoomable: "no",
+      name: "New York"
+    },
+    PA: {
+      zoomable: "no",
+      name: "Pennsylvania"
+    },
+    NJ: {
+      zoomable: "no",
+      name: "New Jersey"
+    },
+    DE: {
+      zoomable: "no",
+      name: "Delaware"
+    },
+    MD: {
+      zoomable: "no",
+      name: "Maryland"
+    },
+    VA: {
+      zoomable: "no",
+      name: "Virginia"
+    },
+    WV: {
+      zoomable: "no",
+      name: "West Virginia"
+    },
+    DC: {
+      zoomable: "no",
+      name: "District of Columbia"
+    },
+    OH: {
+      zoomable: "no",
+      name: "Ohio"
+    },
+    IN: {
+      zoomable: "no",
+      name: "Indiana"
+    },
+    IL: {
+      zoomable: "no",
+      name: "Illinois"
+    },
+    WI: {
+      zoomable: "no",
+      name: "Wisconsin"
+    },
+    MI: {
+      zoomable: "no",
+      name: "Michigan"
+    },
+    NC: {
+      zoomable: "no",
+      name: "North Carolina"
+    },
+    SC: {
+      zoomable: "no",
+      name: "South Carolina"
+    },
+    TN: {
+      zoomable: "no",
+      name: "Tennessee"
+    },
+    KY: {
+      zoomable: "no",
+      name: "Kentucky"
+    },
+    GA: {
+      zoomable: "no",
+      name: "Georgia"
+    },
+    AL: {
+      zoomable: "no",
+      name: "Alabama"
+    },
+    MS: {
+      zoomable: "no",
+      name: "Mississippi"
+    },
+    AR: {
+      zoomable: "no",
+      name: "Arkansas"
+    },
+    LA: {
+      zoomable: "no",
+      name: "Louisiana"
+    },
+    OK: {
+      zoomable: "no",
+      name: "Oklahoma"
+    },
+    MO: {
+      zoomable: "no",
+      name: "Missouri"
+    },
+    KS: {
+      zoomable: "no",
+      name: "Kansas"
+    },
+    IA: {
+      zoomable: "no",
+      name: "Iowa"
+    },
+    NE: {
+      zoomable: "no",
+      name: "Nebraska"
+    },
+    MN: {
+      zoomable: "no",
+      name: "Minnesota"
+    },
+    SD: {
+      zoomable: "no",
+      name: "South Dakota"
+    },
+    ND: {
+      zoomable: "no",
+      name: "North Dakota"
+    },
+    WY: {
+      zoomable: "no",
+      name: "Wyoming"
+    },
+    MT: {
+      zoomable: "no",
+      name: "Montana"
+    },
+    UT: {
+      zoomable: "no",
+      name: "Utah"
+    },
+    CO: {
+      zoomable: "no",
+      name: "Colorado"
+    },
+    AZ: {
+      zoomable: "no",
+      name: "Arizona"
+    },
+    NV: {
+      zoomable: "no",
+      name: "Nevada"
+    },
+    OR: {
+      zoomable: "no",
+      name: "Oregon"
+    },
+    WA: {
+      zoomable: "no",
+      name: "Washington"
+    },
+    CA: {
+      zoomable: "no",
+      name: "California"
+    },
+    ID: {
+      zoomable: "no",
+      name: "Idaho"
+    },
+    TX: {
+      zoomable: "no",
+      name: "Texas"
+    },
+    NM: {
+      zoomable: "no",
+      name: "New Mexico"
+    },
+    GU: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Guam"
+    },
+    VI: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Virgin Islands"
+    },
+    PR: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Puerto Rico"
+    },
+    MP: {
+      hide: "yes",
+      zoomable: "no",
+      name: "Northern Mariana Islands"
+    },
+    AS: {
+      hide: "yes",
+      zoomable: "no",
+      name: "American Samoa"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Bo Vest",
+      lat: 35.035,
+      lng: -92.064
+    },
+    "1": {
+      name: "Stephen Adams",
+      lat: 34.946,
+      lng: -91.979
+    },
+    "2": {
+      name: "Francis Enos",
+      lat: 40.616,
+      lng: -124.202
+    },
+    "3": {
+      name: "Jeff Switowski",
+      lat: 38.558,
+      lng: -121.431
+    },
+    "4": {
+      name: "Trevor Holliday",
+      lat: 37.508,
+      lng: -105.009
+    },
+    "5": {
+      name: "Matthew Johnson",
+      lat: 25.694,
+      lng: -80.428
+    },
+    "6": {
+      name: "George Faucher",
+      lat: 30.61,
+      lng: -81.537
+    },
+    "7": {
+      name: "Jason Howard",
+      lat: 43.479,
+      lng: -111.953
+    },
+    "8": {
+      name: "Joshua Delamarian",
+      lat: 47.926,
+      lng: -116.804
+    },
+    "9": {
+      name: "Travis Young",
+      lat: 37.738,
+      lng: -89.256
+    },
+    "10": {
+      name: "Nate Ginley",
+      lat: 40.008,
+      lng: -85.749
+    },
+    "11": {
+      name: "Mike Alt",
+      lat: 39.475,
+      lng: -76.319
+    },
+    "12": {
+      name: "Sean Connell",
+      lat: 43.371,
+      lng: -70.673
+    },
+    "13": {
+      name: "Ryan Smith",
+      lat: 42.739,
+      lng: -84.408
+    },
+    "14": {
+      name: "Jonathon Alford",
+      lat: 31.47,
+      lng: -90.56
+    },
+    "15": {
+      name: "Collin Campbell",
+      lat: 47.476,
+      lng: -111.328
+    },
+    "16": {
+      name: "Eric Stephenson ",
+      lat: 34.784,
+      lng: -76.902
+    },
+    "17": {
+      name: "Jeff Russell",
+      lat: 35.945,
+      lng: -80.593
+    },
+    "18": {
+      name: "Darren Watkins",
+      lat: 35.052,
+      lng: -84.066
+    },
+    "19": {
+      name: "Michael Carswell",
+      lat: 35.736,
+      lng: -81.549
+    },
+    "20": {
+      name: "Jason Carriveau",
+      lat: 46.794,
+      lng: -96.915
+    },
+    "21": {
+      name: "Jake Poulin",
+      lat: 42.884,
+      lng: -72.253
+    },
+    "22": {
+      name: "Justin Champion",
+      lat: 35.166,
+      lng: -106.636
+    },
+    "23": {
+      name: "Beau Smith",
+      lat: 40.693,
+      lng: -84.242
+    },
+    "24": {
+      name: "Matt Abernathy",
+      lat: 36.966,
+      lng: -94.777
+    },
+    "25": {
+      name: "Gavin Blue",
+      lat: 34.533,
+      lng: -96.221
+    },
+    "26": {
+      name: "Andrew Snodgrass",
+      lat: 45.147,
+      lng: -122.84
+    },
+    "27": {
+      name: "Derek Bowling",
+      lat: 33.663,
+      lng: -81.853
+    },
+    "28": {
+      name: "Michael Guest",
+      lat: 34.444,
+      lng: -82.399
+    },
+    "29": {
+      name: "Kevin Cate",
+      lat: 36,
+      lng: -83.756
+    },
+    "30": {
+      name: "Shawn Russell",
+      lat: 33.172,
+      lng: -96.931
+    },
+    "31": {
+      name: "Tom Hollick",
+      lat: 29.583,
+      lng: -98.207
+    },
+    "32": {
+      name: "Billy Morris",
+      lat: 30.063,
+      lng: -95.165
+    },
+    "33": {
+      name: "Rudi Betancourt",
+      lat: 42.615,
+      lng: -87.828
+    },
+    "34": {
+      name: "Ronald Briggs",
+      lat: 44.032,
+      lng: -104.095
+    },
+    "35": {
+      name: "Colton Crowe",
+      lat: 39.489,
+      lng: -88.171
+    },
+    "36": {
+      name: "Ricardo Mendoza",
+      lat: 41.876,
+      lng: -87.986
+    },
+    "37": {
+      name: "John Mullen",
+      lat: 41.604,
+      lng: -90.354
+    },
+    "38": {
+      name: "Mark Mahan",
+      lat: 32.705,
+      lng: -95.119
+    },
+    "39": {
+      name: "Cory Payton",
+      lat: 38.038,
+      lng: -120.228
+    },
+    "40": {
+      name: "Jeff Adams",
+      lat: 33.902,
+      lng: -117.871
+    },
+    "41": {
+      name: "Devin Trujillo",
+      lat: 33.689,
+      lng: -101.987
+    },
+    "42": {
+      name: "Khalen Howard",
+      lat: 35.225,
+      lng: -106.633
+    },
+    "43": {
+      name: "Victor Short ",
+      lat: 32.15,
+      lng: -110.778
+    },
+    "44": {
+      name: "Brett Willey",
+      lat: 41.3,
+      lng: -111.994
+    }
+  },
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      width: 45
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      width: 45
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      width: 45
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      width: 45
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      width: 45
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      width: 45
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      width: 45
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      width: 45
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      width: 45
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
